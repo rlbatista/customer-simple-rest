7c6fd6f45d98069a96bfa7f4217f68b9f1bf4c37
Renato <sonomel00@yahoo.com.br>
2020-08-19 16:07:26 -0300

    Configs for memory auth.

--- src/main/java/br/com/brasilprev/config/SecurityConfig.java
+++ src/main/java/br/com/brasilprev/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package br.com.brasilprev.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -15,11 +19,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.inMemoryAuthentication()
-			.withUser("test_user")
-			.password("changeMe")
-			.roles("ADMIN");
+			.withUser("test_user").password("changeMe").roles("ADMIN", "USER")
+			.and()
+			.withUser("guest").password("123").roles("USER");
 	}
 	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.csrf()
+        	.disable()
+            .authorizeRequests()
+            	.antMatchers("/customer/**").hasRole("ADMIN")
+            	.anyRequest().authenticated()
+            .and()
+            	.httpBasic()
+            .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+    
 	@Bean
 	@Override
 	protected AuthenticationManager authenticationManager() throws Exception {
