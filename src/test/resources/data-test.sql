66aece7cebb8cae1ccf0150a15089cdb88a66c6d
Renato <sonomel00@yahoo.com.br>
2020-08-18 21:58:45 -0300

    Set a valid CPF on test sql file.

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,2 +1,2 @@
 insert into customers (id, cpf, name, street, number, neighborhood, city, state_code) 
-	values (1, '12345678901', 'Fulano do teste', 'Av Francisco Glicerio', '10A', 'Centro', 'Campinas', 'SP');
+	values (1, '20455988021', 'Fulano do teste', 'Av Francisco Glicerio', '10A', 'Centro', 'Campinas', 'SP');
